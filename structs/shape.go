603e43c0c30140afc2628e8074b2ff8683ebc6f2
tenchael <hztengzz@163.com>
2020-07-05 16:11:05 +0800

    area of circle

--- structs/shape.go
+++ structs/shape.go
@@ -1,12 +1,24 @@
 package structs
 
+import "math"
+
 // Rectangle is one of shapes
-type Rectangle struct{
-	Width float64
+type Rectangle struct {
+	Width  float64
 	Height float64
+}
+
+// Circle is one of shapes
+type Circle struct {
+	Radius float64
 }
 
 // Primeter is used for compute of shape's primeter
 func Primeter(rectangle Rectangle) float64 {
 	return (rectangle.Width + rectangle.Height) * 2
+}
+
+// Area of circle
+func Area(circle Circle) float64 {
+	return circle.Radius * circle.Radius * math.Pi
 }
--- structs/shape_test.go
+++ structs/shape_test.go
@@ -1,11 +1,23 @@
 package structs
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestPrimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
 	actual := Primeter(rectangle)
 	expect := 40.0
+	if actual != expect {
+		t.Errorf("actual '%.2f', but expect '%.2f'", actual, expect)
+	}
+}
+
+func TestArea(t *testing.T) {
+	circle := Circle{10.0}
+	actual := Area(circle)
+	expect := 100 * math.Pi
 	if actual != expect {
 		t.Errorf("actual '%.2f', but expect '%.2f'", actual, expect)
 	}
