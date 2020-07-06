61355ae50a8083f1f68edb0594ad7d29bb185aea
KenTeng <zhizhang.teng@shopee.com>
2020-07-06 11:03:29 +0800

    error handle usage

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,16 +1,41 @@
 package pointers
 
-// Wallet is a electronic coin based on black-chain
+import (
+	"errors"
+	"fmt"
+)
+
+// Bitcoin is one of electronic coin based on black-chain
+type Bitcoin int
+
+// Wallet is package for electronic coin
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // Balance of bitcoin
-func (w Wallet) Balance() int {
+func (w Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
 // Deposit money into wallet
-func (w *Wallet) Deposit(money int) {
-	w.balance += money
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Printf("Current balance is %s", w)
+	w.balance += amount
 }
+
+func (w *Wallet) String() string {
+	return fmt.Sprintf("%d BTC", w.balance)
+}
+
+// Withdraw is used for withdraw amount of money
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficienctFunds
+	}
+	w.balance -= amount
+	return nil
+}
+
+// ErrInsufficienctFunds stands for balance is insufficent
+var ErrInsufficienctFunds = errors.New("balance is not sufficient")
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -3,20 +3,43 @@ package pointers
 import "testing"
 
 func TestBalance(t *testing.T) {
-	wallet := Wallet{10}
-	expect := 10
-	assertEquals(t, wallet.Balance(), expect)
+	wallet := Wallet{Bitcoin(10)}
+	expect := Bitcoin(10)
+	assertBalance(t, wallet.Balance(), expect)
 }
 
 func TestDeposit(t *testing.T) {
-	wallet := Wallet{10}
-	wallet.Deposit(5)
-	expect := 15
-	assertEquals(t, wallet.Balance(), expect)
+	wallet := Wallet{Bitcoin(10)}
+	wallet.Deposit(Bitcoin(5))
+	expect := Bitcoin(15)
+	assertBalance(t, wallet.Balance(), expect)
 }
 
-func assertEquals(t *testing.T, actual, expect int) {
+func TestWithdraw(t *testing.T) {
+	t.Run("Withdraw with sufficient balance", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(10)}
+		wallet.Withdraw(Bitcoin(4))
+		expect := Bitcoin(6)
+		assertBalance(t, wallet.Balance(), expect)
+	})
+
+	t.Run("Withdraw with insufficient balance", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(10)}
+		err := wallet.Withdraw(Bitcoin(12))
+		assertError(t, err, ErrInsufficienctFunds)
+	})
+}
+
+func assertBalance(t *testing.T, actual, expect Bitcoin) {
+	t.Helper()
 	if actual != expect {
 		t.Errorf("actual '%d', but expect '%d'", actual, expect)
+	}
+}
+
+func assertError(t *testing.T, actual, expect error) {
+	t.Helper()
+	if actual != expect {
+		t.Errorf("actual '%s', but expect '%s'", actual, expect)
 	}
 }
