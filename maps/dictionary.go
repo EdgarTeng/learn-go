4977d1ba8ee5b88642d60e8c4781748a91fe4375
KenTeng <zhizhang.teng@shopee.com>
2020-07-06 11:34:00 +0800

    dictionary usage

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,6 +1,23 @@
 package maps
 
+import "errors"
+
+// Dictionary is short for map[string]string
+type Dictionary map[string]string
+
 // Search is used for search a string from map
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+func (d Dictionary) Search(word string) string {
+	return d[word]
 }
+
+// Add used for put a key-value pair to dict
+func (d Dictionary) Add(key, word string) error {
+	if d.Search(key) != "" {
+		return ErrAlreadyExists
+	}
+	d[key] = word
+	return nil
+}
+
+// ErrAlreadyExists stands for already have key in dict
+var ErrAlreadyExists = errors.New("The key already exists in dictionary")
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,17 +3,53 @@ package maps
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is a test string"}
+	t.Run("word exist in dict", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is a test string"}
+		actual := dictionary.Search("test")
+		expect := "this is a test string"
+		assertMessageEquals(t, actual, expect)
+	})
 
-	actual := Search(dictionary, "test")
-	expect := "this is a test string"
-	assertMessageEquals(t, actual, expect)
+	t.Run("word not exist in dict", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is a test string"}
+		actual := dictionary.Search("test123")
+		expect := ""
+		assertMessageEquals(t, actual, expect)
+	})
+}
 
+func TestAdd(t *testing.T) {
+	t.Run("word exist in dict", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is a test string"}
+		err := dictionary.Add("test", "this is another test string")
+		expect := ErrAlreadyExists
+		assertError(t, err, expect)
+	})
+
+	t.Run("word not exist in dict", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is a test string"}
+		err := dictionary.Add("test123", "hhhh")
+		assertNoError(t, err)
+	})
 }
 
 func assertMessageEquals(t *testing.T, actual, expect string) {
 	t.Helper()
 	if actual != expect {
 		t.Errorf("actual '%s', but expect '%s'", actual, expect)
+	}
+}
+
+func assertError(t *testing.T, actual, expect error) {
+	t.Helper()
+	if actual != expect {
+		t.Errorf("actual '%s', but expect '%s'", actual, expect)
+	}
+}
+
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Errorf("have error '%s'", err)
 	}
 }
